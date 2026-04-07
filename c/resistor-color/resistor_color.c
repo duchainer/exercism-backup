f1f6bb364753596270fd022a491a5c56c7df15a5
raph <duchainer@protonmail.com>
2026-04-07 11:34:19 -0400

    DONE c/exercises/resistor-color

--- c/resistor-color/resistor_color.c
+++ c/resistor-color/resistor_color.c
@@ -1,1 +1,7 @@
 #include "resistor_color.h"
+
+resistor_band_t MY_COLORS[] = {BLACK, BROWN, RED,    ORANGE, YELLOW,
+                               GREEN, BLUE,  VIOLET, GREY,   WHITE};
+const resistor_band_t *colors() { return MY_COLORS; }
+
+uint16_t color_code(resistor_band_t color) { return (uint16_t)color; }
